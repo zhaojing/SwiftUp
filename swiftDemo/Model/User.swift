ccb3e999c0bd5ed2dc079baeb1f87ff1338907bd
JingZhao <jingzhao@thoughtworks.com>
2017-10-18 16:41:16 +0800

    add model

---
+++ swiftDemo/Model/User.swift
@@ -0,0 +1,12 @@
+struct User {
+    let id: Int
+    let name: String
+    let activeStatus: Int
+    let age: Int
+    let status: Int
+    let gender: Int
+    let email: String
+    let phone: String
+    let image: String
+    let address: String
+}
