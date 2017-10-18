4095f442b6e8224efa8900c567dfc3d12098d412
JingZhao <jingzhao@thoughtworks.com>
2017-10-18 16:30:48 +0800

    init tableView

--- swiftDemo/ViewController.swift
+++ swiftDemo/ViewController.swift
@@ -1,25 +1,20 @@
-//
-//  ViewController.swift
-//  swiftDemo
-//
-//  Created by JingZhao on 18/10/2017.
-//  Copyright © 2017 JingZhao. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+}
